ed05e52747a024d12f15ae023a2ebeeea745cae6
Vlad-sha <104459120+Vlad-sha@users.noreply.github.com>
2023-02-06 16:18:02 +0400

    Project update

--- react-projects/21-jsonplaceholder-posts/src/components/Post.js
+++ react-projects/21-jsonplaceholder-posts/src/components/Post.js
@@ -1,5 +1,15 @@
-function Post() {
-  return <h1>This is post</h1>
+import "./Post.css"
+
+function Post(props) {
+  const { userId, id, title, body } = props
+  return (
+    <div className="post">
+      <small>{id}</small>
+      <h2>{title}</h2>
+      <p>{body}</p>
+      <h3>User ID : {userId}</h3>
+    </div>
+  )
 }
 
 export default Post
--- react-projects/21-jsonplaceholder-posts/src/components/Posts.js
+++ react-projects/21-jsonplaceholder-posts/src/components/Posts.js
@@ -1,22 +1,37 @@
 import Post from "./Post"
 import { useState, useEffect } from "react"
 
+const API_URL = "https://jsonplaceholder.typicode.com/posts"
+
 function Posts() {
   const [posts, setPosts] = useState([])
+  const [error, setError] = useState("")
+  const [isLoading, setIsLoading] = useState(true)
+
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
+    fetch(API_URL)
       .then((response) => response.json())
-      .then((posts) => {
-        console.log(posts)
-        setPosts(posts)
-      })
-      .catch((error) => console.log(error.message))
+      .then((posts) => setPosts(posts))
+      .catch((error) => setError(error.message))
+      .finally(() => setIsLoading(false))
   }, [])
+
+  if (error) {
+    return <h1>Error: {error}</h1>
+  }
+
   return (
-    <div>
-      <Post />
-      <Post />
-    </div>
+    <>
+      <h1>Posts:</h1>
+      <hr />
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : (
+        posts.map((post) => {
+          return <Post key={post.id} {...post} />
+        })
+      )}
+    </>
   )
 }
 
