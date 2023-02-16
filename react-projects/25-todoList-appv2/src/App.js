2ec09d4058958277e7bd6a163182b8f653f1c03b
Vlad-sha <104459120+Vlad-sha@users.noreply.github.com>
2023-02-16 17:15:58 +0400

    adding clearing and filtering buttons, setting new styles

--- react-projects/25-todoList-appv2/src/App.js
+++ react-projects/25-todoList-appv2/src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react"
 import { v4 as uuidv4 } from "uuid"
 import TodoForm from "./components/Todos/TodoForm"
 import TodoList from "./components/Todos/TodoList"
+import TodosActions from "./components/Todos/TodosActions"
 import "./App.css"
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
   const addTodoHandler = (text) => {
     const newTodo = {
       text: text,
-      check: false,
+      toggle: false,
       id: uuidv4(),
     }
     setTodos([...todos, newTodo])
@@ -19,11 +20,35 @@ function App() {
     setTodos(todos.filter((todo) => todo.id !== id))
   }
 
+  const toggleTodoHandler = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, toggle: !todo.toggle } : { ...todo }
+      )
+    )
+  }
+
+  const resetTodosHandler = () => {
+    setTodos([])
+  }
+
+  const deleteCompletedTodosHandler = () => {
+    setTodos(todos.filter((todo) => !todo.toggle))
+  }
+
   return (
     <div className="App">
       <h1>Todo App</h1>
       <TodoForm handler={addTodoHandler} />
-      <TodoList todos={todos} deleteTodo={deleteHandler} />
+      <TodosActions
+        resetTodos={resetTodosHandler}
+        deleteCompleted={deleteCompletedTodosHandler}
+      />
+      <TodoList
+        todos={todos}
+        deleteTodo={deleteHandler}
+        toggleTodo={toggleTodoHandler}
+      />
     </div>
   )
 }
