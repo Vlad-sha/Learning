33b56102e6d9a5f7884f721ada2d55cdc33ca62d
Vlad-sha <104459120+Vlad-sha@users.noreply.github.com>
2023-03-01 19:22:07 +0400

    Courses project

---
+++ react-projects/26-react-router/src/data/Courses.js
@@ -0,0 +1,24 @@
+const Courses = [
+  {
+    title: "Complete React guide",
+    slug: "react",
+    id: 257,
+  },
+  {
+    title: "Complete JS guide",
+    slug: "javascript",
+    id: 573,
+  },
+  {
+    title: "Complete Python guide",
+    slug: "python",
+    id: 872,
+  },
+  {
+    title: "Complete Node.js guide",
+    slug: "node-js",
+    id: 361,
+  },
+]
+
+export default Courses
