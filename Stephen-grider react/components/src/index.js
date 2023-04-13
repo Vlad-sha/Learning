276e86802066595ad818ce9a26b65cc3c925ca15
Vlad-sha <104459120+Vlad-sha@users.noreply.github.com>
2023-04-13 17:26:29 +0400

    stage 2

--- Stephen-grider react/components/src/index.js
+++ Stephen-grider react/components/src/index.js
@@ -2,29 +2,38 @@ import React from "react"
 import ReactDOM from "react-dom"
 import { faker } from "@faker-js/faker"
 import CommentDetail from "./CommentDetail"
+import ApprovalCard from "./ApprovalCard"
 
 const App = () => {
   //   const image = faker.image.avatar()
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 15:45pm"
-        img={faker.image.avatar()}
-        text="Woohoo"
-      />
-      <CommentDetail
-        author="Alex"
-        timeAgo="Today at 02:00am"
-        img={faker.image.avatar()}
-        text="Love it!"
-      />
-      <CommentDetail
-        author="Clover"
-        timeAgo="Yesterday at 05:00pm"
-        img={faker.image.avatar()}
-        text="Bestie!"
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 15:45pm"
+          img={faker.image.avatar()}
+          text="Woohoo"
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 02:00am"
+          img={faker.image.avatar()}
+          text="Love it!"
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Clover"
+          timeAgo="Yesterday at 05:00pm"
+          img={faker.image.avatar()}
+          text="Bestie!"
+        />
+      </ApprovalCard>
     </div>
   )
 }
