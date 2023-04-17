3cb4862788f8553f742881610005fe643f705419
Vlad-sha <104459120+Vlad-sha@users.noreply.github.com>
2023-04-17 17:45:29 +0400

    seasons 01

---
+++ Stephen-grider react/seasons/src/index.js
@@ -0,0 +1,37 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    // THIS IS THE ONLY TIME WE DO DIRECT ASSIGNMENT
+    // to this.state
+    this.state = { lat: null, errorMessage: "" }
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        // we called the setState!
+        this.setState({ lat: position.coords.latitude })
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message })
+      }
+    )
+  }
+
+  //React says we have to define render!!
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error : {this.state.errorMessage}</div>
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>
+    }
+
+    return <div>Loading!</div>
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<App />)
