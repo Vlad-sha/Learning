86657902a16af630b542d57a729f06f7a6675579
Vlad-sha <104459120+Vlad-sha@users.noreply.github.com>
2023-05-13 15:10:48 +0400

    Starting Widgets proj

---
+++ Stephen-grider react/widgets/src/App.js
@@ -0,0 +1,26 @@
+import Accordion from "./Accordion"
+
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a front end javascript framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite JS library among engineers",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use Reacr by creating components",
+  },
+]
+
+function App() {
+  return (
+    <div>
+      <Accordion items={items} />
+    </div>
+  )
+}
+
+export default App
