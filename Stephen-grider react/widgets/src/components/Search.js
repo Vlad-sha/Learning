04964627777673ff8d4ac50cca61f7bc19afc187
Vlad-sha <104459120+Vlad-sha@users.noreply.github.com>
2023-05-20 13:53:15 +0400

    widgets in progress 3

--- Stephen-grider react/widgets/src/components/Search.js
+++ Stephen-grider react/widgets/src/components/Search.js
@@ -1,7 +1,57 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
+import axios from "axios"
 
 const Search = () => {
   const [text, setText] = useState("")
+  const [results, setResults] = useState([])
+
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: text,
+        },
+      })
+      setResults(data.query.search)
+    }
+
+    if (text && !results.length) {
+      search()
+    } else {
+      const timeoutId = setTimeout(() => {
+        if (text) {
+          search()
+        }
+      }, 700)
+
+      return () => {
+        clearTimeout(timeoutId)
+      }
+    }
+  }, [text])
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    )
+  })
 
   return (
     <div>
@@ -15,6 +65,7 @@ const Search = () => {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   )
 }
