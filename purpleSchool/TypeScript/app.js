95f07224860b2abfd5afa066a616fbe0298ccf3c
Vlad <vladshasamurai@gmail.com>
2024-12-19 13:17:05 +0300

    T0

---
+++ purpleSchool/TypeScript/app.js
@@ -0,0 +1,7 @@
+"use strict";
+function getFullName(firstName, surName) {
+    return `${firstName} ${surName}`;
+}
+const getFullNameArrow = (firstName, surName) => {
+    return `${firstName} ${surName}`;
+};
