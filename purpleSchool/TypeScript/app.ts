3ad3c7c6a39552ff54ed1f0795b9e92a99ab907f
Vlad <vladshasamurai@gmail.com>
2024-12-25 13:19:05 +0300

    Types

--- purpleSchool/TypeScript/app.ts
+++ purpleSchool/TypeScript/app.ts
@@ -5,3 +5,43 @@ function getFullName (firstName : string, surName : string) :string {
 const getFullNameArrow = (firstName : string, surName : string) :string => {
     return `${firstName} ${surName}` ;
 }
+
+
+enum StatusCode {
+    SUCCESS = 1,
+    FAIL = 0,
+    PENDING = 3
+}
+
+const res = {
+    message: "Успешно",
+    statusCode: StatusCode.SUCCESS
+}
+
+if (res.statusCode === StatusCode.SUCCESS) {
+
+}
+
+enum Status {
+	Published = 'published',
+    Draft = 'draft',
+    Deleted = 'deleted'
+}
+
+async function getFaqs(req : {
+	topicId: number,
+	status?: Status
+}) : Promise <{
+question : string,
+answer: string,
+tags : string[],
+likes : number,
+status : Status
+}[]> {
+	const res = await fetch('/faqs', {
+		method: 'POST',
+		body: JSON.stringify(req)
+	});
+	const data = await res.json();
+	return data;
+}
