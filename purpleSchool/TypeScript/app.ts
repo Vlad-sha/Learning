abd207b9c0aafafa59ee802da7196a24b79c7318
Vlad <vladshasamurai@gmail.com>
2025-01-21 18:23:06 +0300

    set/get, implementsntion, extension

--- purpleSchool/TypeScript/app.ts
+++ purpleSchool/TypeScript/app.ts
@@ -17,3 +17,60 @@ class User {
        }
     }
 }
+
+enum PaymentStatus {
+    Holded,
+    Processed,
+    Reversed
+}
+
+
+class Payment {
+    id: number;
+    status: PaymentStatus = PaymentStatus.Holded;;
+    createdAt: Date = new Date();
+    updatedAt: Date;
+
+
+    constructor(id: number) {
+        this.id = id
+        this.status 
+        this.createdAt 
+    }
+
+    getPaymentLifeTime () {
+       return new Date().getTime() - this.createdAt.getTime();
+    }
+    unholdPayment() : void {
+        if (this.status === PaymentStatus.Processed) {
+            throw new Error ('Платеж не может быть возвращён');
+        }
+        this.status = PaymentStatus.Reversed;
+        this.updatedAt = new Date();
+    }
+}
+
+
+class User2 {
+    _login : string
+    password : string
+
+    set login ( e : string) {
+        this._login = 'user' + e 
+    }
+}
+
+interface ILogger {
+    log (...args): void;
+    error (...args): void;
+}
+
+class Log implements ILogger {
+    log(...args: any[]): void {
+        console.log(...args);
+    }
+    error(...args: any[]): void {
+        console.log(...args);
+    }
+    
+}
