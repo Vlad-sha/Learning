1612b8bac74d7353a4eee8932a82f7a2662103a4
Vlad <vladshasamurai@gmail.com>
2025-01-23 18:24:36 +0300

    Cart

--- purpleSchool/TypeScript/app.ts
+++ purpleSchool/TypeScript/app.ts
@@ -1,114 +1,58 @@
-class User {
-    name : string;
-    age : number
+class Product {
+    constructor (
+       public name : string,
+       public id: number,
+       public price: number
+    ) {}
+}
 
-    constructor();
-    constructor(name : string)
-    constructor(age : number)
-    constructor( name : string, age : number)
-    constructor(ageOrName? : string | number, age? : number) {
-       if (typeof ageOrName ==='string') {
-        this.name = ageOrName;
-       } else if (typeof ageOrName === 'number') {
-        this.age = ageOrName
-       }
-       if (typeof age === 'number') {
-        this.age = age
-       }
+class Delivery {
+    constructor (
+        date: Date
+    ) {}
+}
+
+class HomeDelivery extends Delivery {
+    constructor (date : Date, homeAdress : string) {
+        super(date)
     }
 }
 
-enum PaymentStatus {
-    Holded,
-    Processed,
-    Reversed
+
+class AdressDelivery extends Delivery {
+    constructor ( shopId : number) {
+        super(new Date())
+    }
 }
 
+type DeliveryOpt = HomeDelivery | AdressDelivery
 
-class Payment {
-    id: number;
-    status: PaymentStatus = PaymentStatus.Holded;;
-    createdAt: Date = new Date();
-    updatedAt: Date;
-
-
-    constructor(id: number) {
-        this.id = id
-        this.status 
-        this.createdAt 
+class Cart {
+    private products : Product[] = [];
+    private delivery : DeliveryOpt;
+    addProduct (product: Product) {
+        this.products.push(product);
+    }
+    deleteProduct (productId : number) {
+        this.products = this.products.filter((product : Product) => product.id !== productId);
+    }
+    getSum() : number {
+        return this.products
+        .map((p : Product) => p.price)
+        .reduce((price1 : number, price2 : number) => price1 + price2);
     }
 
-    getPaymentLifeTime () {
-       return new Date().getTime() - this.createdAt.getTime();
+    setDelivery (delivery : DeliveryOpt ) : void {
+        this.delivery = delivery;
     }
-    unholdPayment() : void {
-        if (this.status === PaymentStatus.Processed) {
-            throw new Error ('Платеж не может быть возвращён');
+
+    CheckOut () {
+        if (this.products.length == 0) {
+            throw new Error ("Нет товаров в корзине");
         }
-        this.status = PaymentStatus.Reversed;
-        this.updatedAt = new Date();
-    }
-}
-
-
-class User2 {
-    _login : string
-    password : string
-
-    set login ( e : string) {
-        this._login = 'user' + e 
-    }
-}
-
-interface ILogger {
-    log (...args): void;
-    error (...args): void;
-}
-
-class Log implements ILogger {
-    log(...args: any[]): void {
-        console.log(...args);
-    }
-    error(...args: any[]): void {
-        console.log(...args);
-    }
-    
-}
-
-
-class User3 {
-    name : string;
-    constructor (name : string) {
-        this.name = name
-    }
-}
-
-class Users extends Array<User> {
-    searchByname (name: string) {
-        return this.filter(user => user.name === name)
-    }
-
-    override toString(): string {
-        return this.map(user => user.name).join(', ')
-    }
-}
-
-const users = new Users();
-users.push(new User('Артемий'));
-users.push(new User('Эременеджильдо'));
-console.log(users.toString());
-
-
-class Payment2 {
-    date: Date;
-}
-
-class userWithPayment {
-    user: User3
-    payment: Payment2
-
-    constructor(user : User, payment : Payment2) {
-        this.payment = payment;
-        this.user = user
+        if (!this.delivery) {
+            throw new Error ('Не указан способ доставки')
+        }
+        return {success : true};
     }
 }
