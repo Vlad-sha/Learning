265e212ffa29f6d54cb9b5e17b93e9dfd6cd92c9
Vlad <vladshasamurai@gmail.com>
2025-01-29 18:03:32 +0300

    Generics pt.1

--- purpleSchool/TypeScript/src/app.ts
+++ purpleSchool/TypeScript/src/app.ts
@@ -1,19 +1,43 @@
-abstract class Logger {
-    abstract log (message : string) : void
-    printDate (date : Date) {
-        this.log(date.toString());
-    }
+const num : Array<number> = [1,2,3];
+
+async function test () {
+    const v = await new Promise <number> ((resolve, reject) => {
+        resolve(1);
+    });
 }
 
-class RealLogger extends Logger {
-    log (message : string) {
-            console.log (message)
-        }
-    logWithDate (message : string) {
-        this.printDate(new Date());
-        this.log(message);
-    }
+function Middleware<T> (data: T) : T {
+    console.log(data);
+    return data;
 }
 
-const Test = new RealLogger();
-Test.logWithDate('СМС')
+let res = Middleware(10);
+
+function splitHalf <T> (data: Array<T>) : Array<T> {
+    const l = data.length / 2;
+    return data.splice(0,l);
+}
+
+splitHalf<number>([1,2,3,4,5,6]);
+
+
+function Stringify <G> (data : G) : string | undefined {
+    if (typeof data === 'string') {
+        return data
+    } else if (Array.isArray(data)) {
+        return data.toString();
+    }
+    switch (typeof data) {
+        case "number":
+        case "symbol":
+        case "boolean":
+        case "bigint":
+        case "function":
+            return data.toString();
+        case 'object':
+            return JSON.stringify(data);
+        default :
+        return undefined;
+    }
+
+}
