4dd3a506f3adcc64200024145ba35c191ff0949c
Vlad <vladshasamurai@gmail.com>
2025-01-31 18:51:26 +0300

    generics p2 & mixin?!

--- purpleSchool/TypeScript/build/app.js
+++ purpleSchool/TypeScript/build/app.js
@@ -1,17 +1,89 @@
 "use strict";
-class Logger {
-    printDate(date) {
-        this.log(date.toString());
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const num = [1, 2, 3];
+function test() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const v = yield new Promise((resolve, reject) => {
+            resolve(1);
+        });
+    });
+}
+function Middleware(data) {
+    console.log(data);
+    return data;
+}
+let res = Middleware(10);
+function splitHalf(data) {
+    const l = data.length / 2;
+    return data.splice(0, l);
+}
+splitHalf([1, 2, 3, 4, 5, 6]);
+function Stringify(data) {
+    if (typeof data === 'string') {
+        return data;
+    }
+    else if (Array.isArray(data)) {
+        return data.toString();
+    }
+    switch (typeof data) {
+        case "number":
+        case "symbol":
+        case "boolean":
+        case "bigint":
+        case "function":
+            return data.toString();
+        case 'object':
+            return JSON.stringify(data);
+        default:
+            return undefined;
     }
 }
-class RealLogger extends Logger {
-    log(message) {
-        console.log(message);
+const split = splitHalf;
+const logLine = {
+    stamp: new Date(),
+    data: {
+        a: 1
     }
-    logWithDate(message) {
-        this.printDate(new Date());
-        this.log(message);
-    }
+};
+class Vehicle {
 }
-const Test = new RealLogger();
-Test.logWithDate('СМС');
+function kmToMiles(vehicle) {
+    vehicle.run = vehicle.run * 0, 62;
+    return vehicle;
+}
+class LCV extends Vehicle {
+}
+const vehicle = kmToMiles(new Vehicle());
+const lcv = kmToMiles(new LCV());
+function logID(id, additionalData) {
+    console.log(id);
+    console.log(additionalData);
+    return { id, data: additionalData };
+}
+class ObjectSort {
+}
+function Sort(val, type = 'asc') {
+    return val.sort((a, b) => {
+        switch (type) {
+            case 'asc':
+                return a.id - b.id;
+            case 'desc':
+                return b.id + a.id;
+        }
+    });
+}
+const data = [
+    { id: 1, name: " sfds" },
+    { id: 2, name: 'rfvtb' },
+    { id: 3, name: 'plgujhion' }
+];
+console.log(Sort(data, 'asc'));
+console.log(Sort(data, 'desc'));
--- purpleSchool/TypeScript/src/app.ts
+++ purpleSchool/TypeScript/src/app.ts
@@ -41,3 +41,93 @@ function Stringify <G> (data : G) : string | undefined {
     }
 
 }
+
+
+const split : <T> (data : Array<T>) => Array<T> = splitHalf;
+
+
+interface IlogLine <T> {
+    stamp : Date;
+    data : T
+}
+
+type LogLineType <T> = {
+    stamp : Date;
+    data : T
+}
+
+const logLine: IlogLine <{a:number}> = {
+    stamp : new Date(),
+    data : {
+        a : 1
+    }
+}
+
+
+class Vehicle  {
+    run : number
+}
+
+function kmToMiles <T extends Vehicle> (vehicle : T): T {
+    vehicle.run = vehicle.run * 0,62;
+    return vehicle
+}
+
+class LCV extends Vehicle {
+    capacity : number
+}
+
+
+const vehicle = kmToMiles(new Vehicle());
+const lcv = kmToMiles(new LCV());
+
+function logID <T extends string | number, Y> (id : T, additionalData : Y) : {id :T, data : Y} {
+    console.log(id);
+    console.log(additionalData);
+    return {id, data : additionalData}
+}
+
+class ObjectSort {
+    id : number
+}
+
+function Sort < T extends ObjectSort> (val : T[], type: 'asc' | 'desc' = 'asc') : T[] {
+    return val.sort((a,b) => {
+        switch (type) {
+            case 'asc':
+                return a.id - b.id;
+            case 'desc':
+                return b.id + a.id;
+        }
+     })
+}
+
+
+const data = [
+    { id : 1, name : " sfds"},
+    {id : 2, name : 'rfvtb'},
+    {id : 3, name : 'plgujhion'}
+]
+
+console.log(Sort(data, 'asc'));
+console.log(Sort(data, 'desc'));
+
+type GConstructor< T ={}> = new (...args: any[]) => T
+
+class List {
+    constructor(public items  : string[]) {}
+}
+
+type ListType = GConstructor<List>
+
+function ExtendedList<TBase extends ListType>(Base : TBase) {
+    return class ExtendedList extends Base {
+        first() {
+            return this.items[0]
+        }
+    }
+}
+
+const list = ExtendedList(List);
+const res2 = new list(['один',"dva",'tri']);
+console.log(res2.first());
