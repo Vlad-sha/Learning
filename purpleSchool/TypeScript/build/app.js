46f774720e3112a9a6fab45d79c71abbead3b7cf
Vlad <vladshasamurai@gmail.com>
2025-03-14 13:57:32 +0300

    Patterns pt.1 Factory

--- purpleSchool/TypeScript/build/app.js
+++ purpleSchool/TypeScript/build/app.js
@@ -1,13 +1,4 @@
 "use strict";
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __param = (this && this.__param) || function (paramIndex, decorator) {
-    return function (target, key) { decorator(target, key, paramIndex); }
-};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -17,168 +8,67 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-// @nullUsers
-// @setUsers (5)
-// @threeUsersAdvanced
-let UserService = class UserService {
-    constructor() {
-        this.users = 1000;
+class TFInsurance {
+    setVehicle(vehicle) {
+        this.vehicle = vehicle;
     }
-    getUserDataBase() {
-        throw new Error('Ошибка');
-    }
-};
-__decorate([
-    Max(100)
-], UserService.prototype, "users", void 0);
-__decorate([
-    Catch({ rethrow: true }),
-    Log
-], UserService.prototype, "getUserDataBase", null);
-UserService = __decorate([
-    setUsersAdvanced(4),
-    setCreatedAt
-], UserService);
-function Log(target, propertyKey, descriptor) {
-    console.log(target);
-    console.log(propertyKey);
-    console.log(descriptor);
-    descriptor.value = () => {
-        console.log('no error');
-    };
-}
-function Catch({ rethrow } = { rethrow: true }) {
-    return (target, propertyKey, descriptor) => {
-        const oldValue = descriptor.value;
-        descriptor.value = (...args) => __awaiter(this, void 0, void 0, function* () {
-            try {
-                return yield (oldValue === null || oldValue === void 0 ? void 0 : oldValue.apply(target, args));
-            }
-            catch (e) {
-                if (e instanceof Error) {
-                    console.log(e.message);
-                    if (rethrow) {
-                        throw e;
-                    }
-                }
-            }
+    submit() {
+        return __awaiter(this, void 0, void 0, function* () {
+            const res = yield fetch('TF', {
+                method: 'POST',
+                body: JSON.stringify({ vehicle: this.vehicle })
+            });
+            const data = yield res.json();
+            return data.isSuccess;
         });
-    };
+    }
 }
-function Max(max) {
-    return (target, propertyKey) => {
-        let value;
-        const setter = function (newValue) {
-            if (newValue > max) {
-                console.log(`Нельзя установить значение больше ${max}`);
-            }
-            else {
-                value = newValue;
-            }
-        };
-        const getter = function () {
-            return value;
-        };
-        Object.defineProperty(target, propertyKey, {
-            set: setter,
-            get: getter
+class ABInsurance {
+    setVehicle(vehicle) {
+        this.vehicle = vehicle;
+    }
+    submit() {
+        return __awaiter(this, void 0, void 0, function* () {
+            const res = yield fetch('AB', {
+                method: 'POST',
+                body: JSON.stringify({ vehicle: this.vehicle })
+            });
+            const data = yield res.json();
+            return data.isSuccess;
         });
-    };
-}
-function setCreatedAt(constructor) {
-    return class extends constructor {
-        constructor() {
-            super(...arguments);
-            this.createdAt = new Date();
-        }
-    };
-}
-function nullUsers(target) {
-    target.prototype.users = 0;
-}
-function logUsers(obj) {
-    console.log('Users:' + obj.users);
-    return obj;
-}
-console.log(new UserService());
-function threeUsersAdvanced(constructor) {
-    return class extends constructor {
-        constructor() {
-            super(...arguments);
-            this.users = 3;
-        }
-    };
-}
-function setUsers(users) {
-    return (target) => {
-        target.prototype.users = users;
-    };
-}
-function setUsersAdvanced(users) {
-    return (constructor) => {
-        return class extends constructor {
-            constructor() {
-                super(...arguments);
-                this.users = users;
-            }
-        };
-    };
-}
-const userService = new UserService();
-userService.users = 1;
-console.log(userService.users);
-userService.users = 1000;
-console.log(userService.users);
-class UserService2 {
-    set users(num) {
-        this._users = num;
-    }
-    get users() {
-        return this._users;
-    }
-    getUserDataBase() {
-        throw new Error('Ошибка');
     }
 }
-__decorate([
-    Log2()
-], UserService2.prototype, "users", null);
-function Log2() {
-    return (target, _, descriptor) => {
-        const set = descriptor.set;
-        descriptor.set = (...args) => {
-            console.log(args);
-            set === null || set === void 0 ? void 0 : set.apply(target, args);
-        };
-    };
-}
-function Uni(name) {
-    console.log(`Инициализация ${name}`);
-    return function () {
-        console.log(`Вызов ${name}`);
-    };
-}
-let MyClass = class MyClass {
-    static method2(_) { }
-    method(_) { }
-    constructor(_) {
+class InsuranceFactory {
+    saveHistory(ins) {
+        this.db.save(ins.id, ins.status);
     }
+}
+class TFInsuranceFactory extends InsuranceFactory {
+    createInsurance() {
+        return new TFInsurance();
+    }
+}
+class ABInsuranceFactory extends InsuranceFactory {
+    createInsurance() {
+        return new ABInsurance();
+    }
+}
+const tfInsuranceFactory = new TFInsuranceFactory();
+const ins = tfInsuranceFactory.createInsurance();
+tfInsuranceFactory.saveHistory(ins);
+// ALT
+const INSURANCE_TYPE = {
+    tf: TFInsurance,
+    ab: ABInsurance
 };
-__decorate([
-    Uni("Свойство")
-], MyClass.prototype, "props", void 0);
-__decorate([
-    Uni('Метод'),
-    __param(0, Uni('Параметр метода'))
-], MyClass.prototype, "method", null);
-__decorate([
-    Uni("Свойство static")
-], MyClass, "prop2", void 0);
-__decorate([
-    Uni("Метод static"),
-    __param(0, Uni("Параметр метода static"))
-], MyClass, "method2", null);
-MyClass = __decorate([
-    Uni('Класс'),
-    __param(0, Uni('Параметр конструктора'))
-], MyClass);
+class InsuranceFactoryALT {
+    createInsurance(type) {
+        return INSURANCE_TYPE[type];
+    }
+    saveHistory(ins) {
+        this.db.save(ins.id, ins.status);
+    }
+}
+const insuranceFactoryALT = new InsuranceFactoryALT();
+const ins2 = new (insuranceFactoryALT.createInsurance('tf'));
+insuranceFactoryALT.saveHistory(ins2);
--- purpleSchool/TypeScript/src/app.ts
+++ purpleSchool/TypeScript/src/app.ts
@@ -1,190 +1,93 @@
-interface IUserService {
-    users : number;
-    getUserDataBase() : number
+interface IInsurance {
+    id : number
+    status : string
+    setVehicle (vehicle : any) : void;
+    submit() : Promise<boolean>;
 }
 
-
-// @nullUsers
-// @setUsers (5)
-// @threeUsersAdvanced
-@ setUsersAdvanced(4)
-@setCreatedAt
-class UserService implements IUserService {
-    @Max(100)
-    users: number = 1000;
-
-    @Catch({rethrow : true})
-    @Log
-    getUserDataBase(): number {
-
-        throw new Error ('Ошибка')
+class TFInsurance implements IInsurance {
+    id: number;
+    status: string;
+    private vehicle : any
+    setVehicle(vehicle: any): void {
+        this.vehicle = vehicle;
     }
-}
-
-function Log (
-    target : Object,
-    propertyKey : string | symbol,
-    descriptor : TypedPropertyDescriptor<(...args : any[]) => any>
-) : TypedPropertyDescriptor<(...args : any[]) => any> | void {
-    console.log(target);
-    console.log(propertyKey);
-    console.log(descriptor);
-    descriptor.value = () => {
-        console.log('no error');
-    }
-}
-
-function Catch ({rethrow} : {rethrow : boolean} = {rethrow: true}) {
-    return (
-    target : Object,
-    propertyKey : string | symbol,
-    descriptor : TypedPropertyDescriptor<(...args : any[]) => any>
-) : TypedPropertyDescriptor<(...args : any[]) => any> | void => {
-    const oldValue = descriptor.value;
-    descriptor.value = async(...args : any[]) => {
-        try {
-            return await oldValue?.apply(target, args);
-        }
-        catch (e) {
-            if (e instanceof Error) {
-                console.log(e.message);
-                if (rethrow)  {
-                    throw e;
-                }
-            }
-        }
-    }
-}
-}
-
-function Max (max : number) {
-    return (
-        target : Object,
-        propertyKey: string | symbol
-    ) => {
-        let value : number;
-        const setter = function (newValue : number) {
-            if (newValue > max) {
-                console.log(`Нельзя установить значение больше ${max}`);
-            } else {
-                value = newValue;
-            }
-        }
-
-        const getter = function() {
-            return value;
-        }
-        Object.defineProperty(target, propertyKey, {
-            set : setter,
-            get : getter
+    async submit(): Promise<boolean> {
+        const res = await fetch ('TF',{
+            method : 'POST',
+            body : JSON.stringify({vehicle : this.vehicle})
         });
+        const data = await res.json();
+        return data.isSuccess;
     }
 }
 
-function setCreatedAt< T extends { new (...args : any[]): {}}> (constructor : T) {
-    return class extends constructor {
-        createdAt = new Date();
+class ABInsurance implements IInsurance {
+    id: number;
+    status: string;
+    private vehicle : any
+    setVehicle(vehicle: any): void {
+        this.vehicle = vehicle;
+    }
+    async submit(): Promise<boolean> {
+        const res = await fetch ('AB',{
+            method : 'POST',
+            body : JSON.stringify({vehicle : this.vehicle})
+        });
+        const data = await res.json();
+        return data.isSuccess;
     }
 }
 
-function nullUsers (target : Function) {
-    target.prototype.users = 0;
-}
+abstract class InsuranceFactory {
+    db : any;
+    abstract createInsurance() : IInsurance;
 
-function logUsers (obj : UserService) {
-    console.log('Users:' + obj.users);
-    return obj
-}
-
-console.log(new UserService());
-
-
-function threeUsersAdvanced<T extends {new (...args : any[]): {}}> (constructor : T) {
-    return class extends constructor {
-        users = 3;
-    }
-} 
-
-function setUsers (users : number) {
-    return (target : Function) => {
-        target.prototype.users = users;
+    saveHistory (ins : IInsurance) {
+        this.db.save(ins.id, ins.status);
     }
 }
 
-function setUsersAdvanced (users : number) {
-    return <T extends {new (...args : any[]): {} }> (constructor : T) => {
-        return class extends constructor {
-            users = users;
-        }
-    }
-}
-
-const userService = new UserService();
-userService.users = 1;
-console.log(userService.users);
-userService.users = 1000;
-console.log(userService.users);
-
-
-interface IUserService2 {
-    getUserDataBase() : number
-}
-
-class UserService2 implements IUserService2 {
-   private _users: number;
-
-   @Log2()
-   set users (num : number) {
-    this._users = num;
-   }
-
-   get users () {
-    return this._users;
-   }
-
-    getUserDataBase(): number {
-
-        throw new Error ('Ошибка')
-    }
-}
-
-function Log2 () {
-    return (
-        target : Object,
-        _ : string | symbol,
-        descriptor : PropertyDescriptor
-    ) => {
-    const set = descriptor.set
-    descriptor.set = (...args : any) => {
-        console.log(args);
-        set?.apply(target, args);
-    }
-    }
-}
-
-function Uni (name : string) : any {
-    console.log(`Инициализация ${name}`);
-    return function () {
-        console.log(`Вызов ${name}`);
-    }
-} 
-
-@Uni('Класс')
-class MyClass {
-    @Uni ("Свойство")
-    props?: any;
-
-    @Uni("Свойство static")
-    static prop2?: any;
-
-    @Uni("Метод static")
-    static method2 (@Uni("Параметр метода static") _: any) {}
-
-    @Uni('Метод')
-    method (@Uni('Параметр метода') _: any) {}
-
-    constructor (@Uni('Параметр конструктора') _: any) {
-        
+class TFInsuranceFactory extends InsuranceFactory {
+    createInsurance(): TFInsurance {
+        return new TFInsurance();
     }
 
 }
+
+class ABInsuranceFactory extends InsuranceFactory {
+    createInsurance(): ABInsurance {
+        return new ABInsurance();
+    }
+
+}
+
+const tfInsuranceFactory = new TFInsuranceFactory();
+const ins = tfInsuranceFactory.createInsurance();
+tfInsuranceFactory.saveHistory(ins);
+
+// ALT
+
+
+const INSURANCE_TYPE = {
+    tf : TFInsurance,
+    ab : ABInsurance
+};
+
+type IT = typeof INSURANCE_TYPE;
+
+ class InsuranceFactoryALT {
+    db : any;
+    
+    createInsurance<T extends keyof IT>(type : T) : IT[T]{
+        return INSURANCE_TYPE[type];
+    }
+
+    saveHistory (ins : IInsurance) {
+        this.db.save(ins.id, ins.status);
+    }
+ }
+
+ const insuranceFactoryALT = new InsuranceFactoryALT();
+ const ins2 = new (insuranceFactoryALT.createInsurance('tf'));
+ insuranceFactoryALT.saveHistory(ins2);
