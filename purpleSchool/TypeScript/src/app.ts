1d1737c8c277e21eb0e78a1db59e829325c1408c
Vlad <vladshasamurai@gmail.com>
2025-03-17 13:52:36 +0300

    Patterns pt.2 Prototype (clone)

--- purpleSchool/TypeScript/src/app.ts
+++ purpleSchool/TypeScript/src/app.ts
@@ -1,36 +1,22 @@
-class MyMap {
-    private static instance : MyMap;
-    map : Map<number,string> = new Map();
-
-    private constructor () {
-    }
-
-    clean() {
-        this.map = new Map();
-    }
-    public static get() : MyMap {
-        if (!MyMap.instance) {
-            MyMap.instance = new MyMap();
-        }
-        return MyMap.instance;
-    }
+interface Prototype<T> {
+    clone() : T;
 }
 
-class Service1 {
-    addmap (key : number, value : string) {
-        const myMap = MyMap.get();
-        myMap.map.set(key,value);
+class UserHistory implements Prototype<UserHistory> {
+    createdAt : Date
+    constructor(public email: string, public name: string) {
+        this.createdAt = new Date();
     }
+
+    clone(): UserHistory {
+        let target = new UserHistory(this.email,this.name)
+        target.createdAt = this.createdAt;
+        return target;
+    }
+
 }
 
-class Service2 {
-    getKeys(key : number) {
-        const myMap = MyMap.get();
-        console.log(myMap.map.get(key));
-        myMap.clean();
-        console.log(myMap.map.get(key));
-    }
-}
-
-new Service1().addmap(1, 'работает?');
-new Service2().getKeys(1);
+let user = new UserHistory('adasd@ru',"Василий");
+console.log(user);
+let user2 = user.clone;
+console.log(user2);
